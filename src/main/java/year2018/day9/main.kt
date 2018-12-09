7f0fcb5bfb519987485b45d3438e324b9337e930
Tim Siebels <tim_siebels_aurich@yahoo.de>
2018-12-09 15:21:13 +0100

    Day 9 Part 2

--- src/main/java/year2018/day9/main.kt
+++ src/main/java/year2018/day9/main.kt
@@ -1,52 +1,86 @@
 package year2018.day9
 
 import year2018.base.BaseSolution
-import kotlin.math.max
 
 typealias PlayerCount = Int
 typealias Points = Int
-class Solution : BaseSolution<Pair<PlayerCount, Points>, Int, Int>("Day 9") {
+class Solution : BaseSolution<Pair<PlayerCount, Points>, Long, Long>("Day 9") {
     override fun parseInput(): Pair<PlayerCount, Points> = loadInput().let {
         val (players, points) = Regex("""(\d+) players; last marble is worth (\d+) points""").find(it)?.destructured ?: throw IllegalArgumentException("Regex didnt match")
         Pair(players.toInt(), points.toInt())
     }
 
-    override fun calculateResult1(): Int {
+    override fun calculateResult1(): Long {
         val (players, lastPoints) = parseInput()
-//        val (players, lastPoints) = arrayOf(10, 1618)
+        return solve(players, lastPoints)
+    }
+
+    override fun calculateResult2(): Long {
+        val (players, lastPoints) = parseInput()
+        return solve(players, lastPoints * 100)
+    }
+
+    private fun solve(players: PlayerCount, lastPoints: Points): Long {
         val nextPlayer = generateSequence(1) { ((it + 1) % players) }.iterator()
-        val marbles = generateSequence(2) { i -> i + 1 }.take(lastPoints)
-        val board = mutableListOf<Int>(0, 1)
-        var currentIdx = 1
-        val playerScores = IntArray(players)
+        val marbles = generateSequence(2L) { i -> i + 1 }.take(lastPoints)
+        val board = Queue(0L)
+        var current = board.add(1)
+        val playerScores = LongArray(players)
 
         for (currentMarble in marbles) {
             val currentPlayer = nextPlayer.next()
-            if (currentMarble % 23 == 0) {
-                currentIdx = (currentIdx - 7).let {
-                    if (it < 0) {
-                        board.size + it
-                    } else {
-                        it
-                    }
+            if (currentMarble % 23 == 0L) {
+                for (i in 1..7) {
+                    current = current.prev
                 }
-                playerScores[currentPlayer] += board.removeAt(currentIdx) + currentMarble
+                playerScores[currentPlayer] += current.value + currentMarble
+                board.remove(current)
+                current = current.next
             } else {
-                currentIdx = (currentIdx + 2) % (board.size)
-                if (currentIdx == 0) {
-                    currentIdx = board.size
-                    board.add(currentMarble)
-                } else {
-                    board.add(currentIdx, currentMarble)
-                }
+                current = board.addAfter(current.next, currentMarble)
             }
         }
 
         return playerScores.max()!!
     }
+}
 
-    override fun calculateResult2(): Int {
-        return -1
+class Queue<T>(initial: T) {
+    class Node<T>(var value: T) {
+        var next: Node<T> = this
+        var prev: Node<T> = this
+    }
+
+    var start: Node<T> = Node<T>(initial)
+
+    fun addAfter(before: Node<T>, value: T): Node<T> {
+        val node = Node<T>(value)
+        before.next.prev = node
+        node.next = before.next
+        before.next = node
+        node.prev = before
+
+        return node
+    }
+
+    fun add(value: T): Node<T> {
+        return addAfter(start.prev, value)
+    }
+
+    fun remove(node: Node<T>) {
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    }
+
+    fun toList(): List<T> {
+        val ret = mutableListOf<T>()
+        var current = start
+        do {
+            ret += current.value
+            current = current.next
+        } while (current != start)
+
+        return ret
     }
 }
 
