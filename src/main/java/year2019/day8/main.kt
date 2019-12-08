03fcfceecf90296a2c55dc74eb31b35ad8e7fd08
Tim Siebels <siebels.tim@gmail.com>
2019-12-08 19:35:05 +0100

    Y2019D8

---
+++ src/main/java/year2019/day8/main.kt
@@ -0,0 +1,67 @@
+package year2019.day8
+
+import base.BaseSolution
+import base.Coordinate
+import com.github.ajalt.mordant.TermColors
+
+class Solution : BaseSolution<Solution.Image, Int, Int>("Day 8") {
+    data class Image(val pixels: Map<Coordinate, List<Int>>, val width: Int, val height: Int) {
+        val layers: List<List<Int>>
+            get() {
+                val layerCount = pixels.entries.first().value.size
+                return List(layerCount) { layerNumber ->
+                    pixels.entries.map { it.value[layerNumber] }
+                }
+            }
+    }
+
+    override fun parseInput(): Image {
+        val width = 25
+        val height = 6
+        val pixels = mutableMapOf<Coordinate, List<Int>>()
+        val lines = loadInput().toCharArray().map { it.toString().toInt() }.chunked(width)
+        for ((lineNo, line) in lines.withIndex()) {
+            val y = lineNo % height
+            for ((x, value) in line.withIndex()) {
+                val pixel = pixels.computeIfAbsent(Coordinate(x, y)) {
+                    emptyList()
+                }
+                pixels[Coordinate(x ,y)] = pixel + value
+            }
+        }
+
+        return Image(pixels, width, height)
+    }
+
+    override fun calculateResult1(): Int {
+        val input = parseInput()
+        val layers = input.layers
+        return layers
+                .minBy { layer -> layer.count { it == 0 } }!!
+                .let { layer ->
+                    layer.count { it == 1 } * layer.count { it == 2 }
+                }
+    }
+
+    override fun calculateResult2(): Int {
+        val t = TermColors()
+        val input = parseInput()
+        for (y in 0 until input.height) {
+            for (x in 0 until input.width) {
+                val color = input.pixels[Coordinate(x, y)]!!.first { it != 2 }
+                val symbol = when (color) {
+                    0 -> t.gray("X")
+                    1 -> t.white("O")
+                    else -> IllegalArgumentException("Pixel value invalid: $color")
+                }
+                print(symbol)
+            }
+            println()
+        }
+        return 0
+    }
+}
+
+fun main() {
+    Solution().solveWithMeasurement()
+}
